4140b5fa1a4123b02f4d305ba266bfce0d9f1277
Yurly123 <joo7won@gmail.com>
2025-05-10 16:10:35 +0900

    `checkGameEnd`, `floodFill` 함수 구현

--- gameLoop.c
+++ gameLoop.c
@@ -32,10 +32,50 @@ void handleGameInput(struct Cell** board, enum Difficulty difficulty) {
 
 // 박ㅈㅇ
 void floodFill(struct Cell** board, int x, int y, enum Difficulty difficulty) {
-  
+  if (board[x][y].adjacentMines != 0)
+    return;
+
+  int rows, cols;
+  switch (difficulty) {
+    case EASY: rows = 9; cols = 9; break;
+    case MEDIUM: rows = 16; cols = 16; break;
+    case HARD: rows = 16; cols = 30; break;
+  }
+
+  for (int i = x - 1; i <= x + 1; i++) {
+    for (int j = y - 1; j <= y + 1; j++) {
+      if (i < 0 || i >= rows || j < 0 || j >= cols)
+        continue;
+      if (board[i][j].isRevealed)
+        continue;
+
+      board[i][j].isRevealed = 1;
+      floodFill(board, i, j, difficulty);
+    }
+  }
 }
 
 // 박ㅈㅇ
 enum GameState checkGameEnd(struct Cell** board, enum Difficulty difficulty) {
+  int rows, cols, mines;
+  switch (difficulty) {
+    case EASY: rows = 9; cols = 9; mines = 10; break;
+    case MEDIUM: rows = 16; cols = 16; mines = 40; break;
+    case HARD: rows = 16; cols = 30; mines = 99; break;
+  }
+
+  int leftCells = rows * cols - mines;
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      struct Cell cell = board[i][j];
+      if (cell.isRevealed && cell.isMine)
+        return GAME_OVER;
+      if (cell.isRevealed && !cell.isMine)
+        leftCells--;
+    }
+  }
   
+  if (leftCells == 0)
+    return WIN;
+  else return PLAYING;
 }
